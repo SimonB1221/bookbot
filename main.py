7321da1b4bf4494ccf4b677f79ac8c3e7b00e3d4
simon_blome <simon_blome@hotmail.de>
2025-06-27 04:44:05 +0200

    added function to count each word

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_symbols
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +9,8 @@ def get_book_text(filepath):
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    word_count = count_words(text)
-    print(f"{word_count} words found in the document")
-
+    #word_count = count_words(text)
+    #print(f"{word_count} words found in the document")
+    #symbol_count = count_symbols(text)
+    print(count_symbols(text))
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,15 @@ def count_words(text):
     return counter
 
 
+def count_symbols(text):
+    words_dict = {}
+    low_text = text.lower()
+    words_list = low_text.split()
+    words_set = set(words_list)
+    for word in words_list:
+        if word in words_dict:
+            words_dict[word] = words_dict[word] + 1
+        else:
+            words_dict[word] = 1
+    test = words_dict
+    return test
