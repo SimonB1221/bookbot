cc11c965f9de58ddab9e46f41571e990061010d9
simon_blome <simon_blome@hotmail.de>
2025-06-27 04:08:07 +0200

    made analysis tool and optimized it

---
+++ main.py
@@ -0,0 +1,14 @@
+from stats import count_words
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    word_count = count_words(text)
+    print(f"{word_count} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,8 @@
+def count_words(text):
+    words = text.split()
+    counter = 0
+    for word in words:
+        counter +=1
+    return counter
+
+
